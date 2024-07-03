79fae5a7450893291db1c5344df16c34026e8ef9
Lennard978 <shparavalo78nenad@gmail.com>
2024-07-03 19:48:12 +0200

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,11 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "liza",
   plugins: [react(), VitePWA({ registerType: 'autoUpdate' })], server: {
   }, registerType: "autoUpdate",
   devOptions: {
     enabled: true,
   },
-  "scripts": {
-    "build": "vite build",
-    "preview": "vite preview"
-  }
+
 })
